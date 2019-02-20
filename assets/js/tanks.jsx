02bd21903970489c0ab9657511fed96455d8c42f
tsengjonathan <tseng.j@husky.neu.edu>
2019-02-20 16:13:30 -0500

    Migrated to webpack and installed front end dependencies

---
+++ assets/js/tanks.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function hangman_init(root, channel) {
+  ReactDOM.render(<Tanks channel={channel} />, root);
+}
+
+class Tanks extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return <div>Hello World!</div>
+    }
+}
