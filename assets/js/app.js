4834803adaf472392294268fa0c26f360f3605a8
tsengjonathan <tseng.j@husky.neu.edu>
2019-02-21 12:59:29 -0500

    Linted project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,29 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.scss";
+import $ from 'jquery';
+// eslint-disable-next-line no-unused-vars
+import css from '../css/app.scss';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
-// in "webpack.config.js".
+// in 'webpack.config.js'.
 //
 // Import dependencies
 //
-import "phoenix_html";
-import $ from "jquery";
-import "bootstrap";
+import 'phoenix_html';
+import 'bootstrap';
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-import socket from "./socket";
-import tanks_init from "./tanks";
+import socket from './socket';
+import tanks from './tanks';
 
 $(() => {
-  let root = document.getElementById('root');
+  const root = document.getElementById('root');
   if (root) {
-    let channel = socket.channel("game_session:" + window.gameName, {});
-    tanks_init(root, channel);
+    const channel = socket.channel(`game_session:${window.gameName}`, {});
+    tanks(root, channel);
   }
 });
